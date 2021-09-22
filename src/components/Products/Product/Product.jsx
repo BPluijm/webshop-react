2a9e408c29fe45a68f9fedc07e7ccb89cd7a734d
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-09-22 19:32:44 +0200

    added navbar

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,7 +9,7 @@ function Product({ product }) {
 
     return (
         <Card className={classes.root}>
-            <CardMedia classname={classes.media} image='' title={product.name} />
+            <CardMedia classname={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
@@ -19,7 +19,7 @@ function Product({ product }) {
                         {product.price}
                     </Typography>
                 </div>
-                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
                 <IconButton aria-label="Add to Cart">
